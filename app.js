e80fbc9e839cedbc1f079acff4c6470ef233e90b
sp381 <sarah.powell381@gmail.com>
2022-03-20 15:39:25 -0700

    learning Node

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var message = 'Hello Node!';
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-var sum = 5 + 3;
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    // This... 
+    for (let i = 0; i < profileDataArr.length; i+= 1) {
+      console.log(profileDataArr[i]);
+    }
 
-console.log(message);
-console.log(sum);
+    console.log('================');
+
+    //Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+
+printProfileData(profileDataArgs);
